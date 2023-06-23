ceecbd33c0b0d898e5e00cf39b9f46cb649a1de2
Wunmi Sogunle <sogunledolapo@gmail.com>
2023-06-23 20:19:48 +0100

    finish styles

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import classes from "./Button.module.css";
+import { ReactComponent as AIIcon } from "../../assets/ai.svg";
+import { ReactComponent as ReplyIcon } from "../../assets/reply.svg";
 
 const sizes = {
   small: classes.smallButton,
@@ -7,9 +9,29 @@ const sizes = {
   large: classes.largeButton,
 };
 
-export const Button = ({ text, onClick, size = "large" }) => {
+const types = {
+  filled: classes.filled,
+  border: classes.border,
+};
+
+const icons = {
+  ai: <AIIcon />,
+  reply: <ReplyIcon />,
+};
+
+export const Button = ({
+  text,
+  onClick,
+  icon,
+  type = "filled",
+  size = "large",
+}) => {
   return (
-    <button className={`${classes.button} ${sizes[size]}`} onClick={onClick}>
+    <button
+      className={`${classes.button} ${sizes[size]} ${types[type]}`}
+      onClick={onClick}
+    >
+      <span className={classes.icon}>{icon && icons[icon]}</span>
       <span>{text}</span>
     </button>
   );
--- frontend/src/components/EmailReply/EmailReply.jsx
+++ frontend/src/components/EmailReply/EmailReply.jsx
@@ -1,21 +1,41 @@
 import React, { useState } from "react";
-import classes from "./EmailContent.module.css";
+import classes from "./EmailReply.module.css";
 import { Button } from "../Button/Button";
-import { EmailReply, EmailTextArea } from "../EmailReply/EmailReply";
+import { ReactComponent as CancelIcon } from "../../assets/cancel.svg";
 
-export const EmailContent = ({ email }) => {
+export const EmailTextArea = ({ onCancel }) => {
+  return (
+    <div className={classes.emailReply}>
+      <button className={classes.cancelButton} onClick={onCancel}>
+        <CancelIcon />
+      </button>
+      <textarea
+        className={classes.textArea}
+        placeholder='Write your reply here...'
+      ></textarea>
+      <button className={classes.replyButton}>Reply</button>
+    </div>
+  );
+};
+
+export const EmailReply = ({ email }) => {
   const [showReply, setShowReply] = useState(false);
+  if (!email) return null;
   return (
     <div className={classes.container}>
       <div className={classes.header}>
         <div className={classes.sender}>
-          <span>{email.senderName}</span>
+          <span>
+            {email.senderName}{" "}
+            <span className={classes.autoReply}>
+              {email.isAutoReply ? "(Auto-Reply)" : ""}
+            </span>
+          </span>
           <span className={classes.dateTime}>{email.dateTime}</span>
         </div>
-        <div className={classes.subject}>{email.subject}</div>
-        {/* <div className={classes.to}>To: {email.recipients.join(", ")}</div> */}
+
         <div className={classes.to}>
-          To: {Array.isArray(email.to) ? email.to.join(", ") : ""}
+          To: {Array.isArray(email.to) ? email.to.join(", ") : "Me"}
         </div>
       </div>
 
@@ -28,18 +48,17 @@ export const EmailContent = ({ email }) => {
         Email snippet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
         Email snippet lorem ipsum dolor sit amet lorem ipsum dolor sit amet */}
       </div>
+
       <div className={classes.buttons}>
         <Button
           icon='reply'
           type='border'
           text='Reply'
           size='smallButton'
-          onClick={() => setShowReply(true)}
+          onClick={() => setShowReply(!showReply)}
         />
-        <Button icon='ai' type='border' text='AI Reply' size='smallButton' />
       </div>
 
-      {email.reply ? <EmailReply email={email.reply} /> : null}
       {showReply && <EmailTextArea onCancel={() => setShowReply(!showReply)} />}
     </div>
   );
